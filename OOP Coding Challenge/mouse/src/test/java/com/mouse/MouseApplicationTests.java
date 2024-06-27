efc050d565865aa4a6348f0979b98ac5ca49ae32
nguyenvoankhoa <nguyenvoankhoa@gmail.com>
2024-06-27 08:36:05 +0700

    update code and mindmap mouse challenge

--- OOP Coding Challenge/mouse/src/test/java/com/mouse/MouseApplicationTests.java
+++ OOP Coding Challenge/mouse/src/test/java/com/mouse/MouseApplicationTests.java
@@ -2,19 +2,22 @@ package com.mouse;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
 @SpringBootTest
 class MouseApplicationTests {
-
-
-    @Test
-    void contextLoads() {
+    Mouse mouse;
+    List<TargetObject> objects;
+    @BeforeEach
+    public void setUp(){
+        objects = new ArrayList<>();
         List<Light> lights = new ArrayList<>();
         Light light = new Light("Logitech", "black", 100, null);
         lights.add(light);
@@ -23,36 +26,66 @@ class MouseApplicationTests {
         List<Point> points = new ArrayList<>();
         points.add(point);
         List<Button> buttons = new ArrayList<>();
-        Button left = new Button("Left",
-                new Shape(points, true, "black", new Line("black", 0)));
-        Button right = new Button("Right",
-                new Shape(points, true, "black", new Line("black", 0)));
-        Button next = new Button("Next",
-                new Shape(points, true, "black", new Line("black", 0)));
-        Button back = new Button("Back",
-                new Shape(points, true, "black", new Line("black", 0)));
+        Button left = new Button("Left", new Shape(points, true, "black"));
+        Button right = new Button("Right", new Shape(points, true, "black"));
+        Button next = new Button("Next", new Shape(points, true, "black"));
+        Button back = new Button("Back", new Shape(points, true, "black"));
         buttons.add(left);
         buttons.add(right);
         buttons.add(next);
         buttons.add(back);
         Position position = new Position(0, 0);
-        Mouse mouse = new Mouse(lights, buttons, wheel, position);
+        mouse = new Mouse(lights, buttons, wheel, position, 50);
+    }
 
+    @Test
+    public void testMove(){
+        Position position = new Position(2, 3);
+        mouse.move(2, 3);
+        assert(position.equals(mouse.position));
+    }
+    @Test
+    public void testAdjustSensitivity(){
+        int newSensitivity = 100;
+        mouse.adjustSensitivity(newSensitivity);
+        assert(mouse.sensitivity == newSensitivity);
+    }
+    @Test
+    public void testClick(){
 
+    }
+
+    public class TargetObject {
+        int xLeft;
+        int xRight;
+        int yTop;
+        int yBottom;
+        String name;
+
+        public TargetObject(int xLeft, int xRight, int yTop, int yBottom, String name) {
+            this.xLeft = xLeft;
+            this.xRight = xRight;
+            this.yTop = yTop;
+            this.yBottom = yBottom;
+            this.name = name;
+        }
     }
 
 
     public class Mouse {
-        public List<Light> light;
+        public List<Light> lights;
         public List<Button> buttons;
         public Wheel wheel;
         public Position position;
 
-        public Mouse(List<Light> light, List<Button> buttons, Wheel wheel, Position position) {
-            this.light = light;
+        public int sensitivity;
+
+        public Mouse(List<Light> lights, List<Button> buttons, Wheel wheel, Position position, int sensitivity) {
+            this.lights = lights;
             this.buttons = buttons;
             this.wheel = wheel;
             this.position = position;
+            this.sensitivity = sensitivity;
         }
 
         public Position move(int xOffset, int yOffset) {
@@ -60,27 +93,14 @@ class MouseApplicationTests {
             this.position.y += yOffset;
             return this.position;
         }
-        public void click(String type) {
-            for (Button btn : buttons) {
-                if (btn.type == type) {
-                    btn.action("click");
-                }
-            }
+
+        public int adjustSensitivity(int newSensitivity) {
+            this.sensitivity = newSensitivity;
+            return sensitivity;
         }
-        public void doubleClick(String type) {
-            for (Button btn : buttons) {
-                if (btn.type == type) {
-                    btn.action("double click");
-                }
-            }
-        }
-        public void press(String type) {
-            for (Button btn : buttons) {
-                if (btn.type == type) {
-                    btn.action("press");
-                }
-            }
-        }
+
+
+
     }
 
     public class Light {
@@ -97,16 +117,20 @@ class MouseApplicationTests {
             this.opacity = opacity;
             this.shape = shape;
         }
-        public void turnOn(){
+
+        public void turnOn() {
             this.status = true;
         }
-        public void turnOff(){
+
+        public void turnOff() {
             this.status = false;
         }
-        public void changeColor(String color){
+
+        public void changeColor(String color) {
             this.color = color;
         }
-        public void changeOpacity(int opacity){
+
+        public void changeOpacity(int opacity) {
             this.opacity = opacity;
         }
     }
@@ -116,16 +140,13 @@ class MouseApplicationTests {
         public boolean fill;
         public String color;
 
-        public Line line;
-
         public Shape() {
         }
 
-        public Shape(List<Point> points, boolean fill, String color, Line line) {
+        public Shape(List<Point> points, boolean fill, String color) {
             this.points = points;
             this.fill = fill;
             this.color = color;
-            this.line = line;
         }
     }
 
@@ -142,19 +163,11 @@ class MouseApplicationTests {
 
     }
 
-    public class Line {
-        public String color;
-        public int width;
-
-        public Line(String color, int width) {
-            this.color = color;
-            this.width = width;
-        }
-    }
 
     public class Button {
         public String type;
         public Shape shape;
+
         public Position position;
 
         public Button(String type, Shape shape) {
@@ -162,14 +175,43 @@ class MouseApplicationTests {
             this.shape = shape;
         }
 
-        public void action(String action) {
-            System.out.println("Button " + type + action);
+        public void click() {
+            System.out.println("Button " + type + " click");
+            for (TargetObject targetObject : objects) {
+                if (checkInRange(targetObject.xLeft, targetObject.xRight, targetObject.yTop, targetObject.yBottom)) {
+                    System.out.println("Button " + type + " click at object " + targetObject.name);
+                }
+            }
+        }
+
+        public void doubleClick() {
+            System.out.println("Button " + type + " double click");
+            for (TargetObject targetObject : objects) {
+                if (checkInRange(targetObject.xLeft, targetObject.xRight, targetObject.yTop, targetObject.yBottom)) {
+                    System.out.println("Button " + type + " double click at object " + targetObject.name);
+                }
+            }
+        }
+
+        public void press() {
+            System.out.println("Button " + type + " press");
+            System.out.println("Button " + type + " double click");
+            for (TargetObject targetObject : objects) {
+                if (checkInRange(targetObject.xLeft, targetObject.xRight, targetObject.yTop, targetObject.yBottom)) {
+                    System.out.println("Button " + type + " double click at object " + targetObject.name);
+                }
+            }
+        }
+
+        public boolean checkInRange(int xLeft, int xRight, int yTop, int yBottom) {
+            return position.x >= xLeft && position.x <= xRight && position.y <= yTop && position.y >= yBottom;
         }
 
     }
 
     public class Wheel {
         public Shape shape;
+
         public Wheel(Shape shape) {
             this.shape = shape;
         }
@@ -199,6 +241,15 @@ class MouseApplicationTests {
                     ", y=" + y +
                     '}';
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Position)) return false;
+            Position position = (Position) o;
+            return x == position.x && y == position.y;
+        }
+
     }
 
 }
